7cbbae2b3f1318a75d1bc9b34d3f74276198caaf
CaioVitor1 <caiovitor33@hotmail.com>
2022-09-26 12:17:25 -0300

    add route e2e

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,11 +2,16 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+import e2eRouter from "../src/routers/e2eRouter.js"
 import recommendationRouter from "./routers/recommendationRouter.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+if(process.env.NODE_ENV === "test") {
+    app.use(e2eRouter);
+}
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
